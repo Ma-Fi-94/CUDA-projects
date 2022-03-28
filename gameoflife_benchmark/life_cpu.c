426ac01db5200318bc1dc0e1af3f30502971645b
Ma-Fi-94 <33027321+Ma-Fi-94@users.noreply.github.com>
2022-03-29 00:34:07 +0200

    Add files via upload

---
+++ gameoflife_benchmark/life_cpu.c
@@ -0,0 +1,67 @@
+// Simulation parameters
+#define XSIZE 1000
+#define YSIZE 1000
+#define MAXSTEP 10000
+
+#include <time.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+void propagate_and_update(int *lattice, int* lattice_new) {
+    for (int x = 0; x < XSIZE; x++) {
+        for (int y = 0; y < YSIZE; y++) { 
+            int i = YSIZE * x + y;
+            lattice_new[i] = lattice[i];
+            
+            if (x > 0 & y > 0 & x < XSIZE-1 & y < YSIZE-1) {
+                int n = lattice[i-YSIZE-1] + lattice[i-YSIZE] + lattice[i-YSIZE+1] +
+                        lattice[i-1] + lattice[i+1] + 
+                        lattice[i+YSIZE-1] + lattice[i+YSIZE] + lattice[i+YSIZE+1];
+                
+                if (lattice[i] == 0 & n == 3) {
+                    lattice_new[i] = 1;
+                } else if (lattice[i] == 1 & n < 2) {
+                    lattice_new[i] = 0;
+                } else if (lattice[i] == 1 & n > 3) {
+                    lattice_new[i] = 0;
+                }        
+            }
+        }        
+    }   
+    
+    for (int i = 0; i < XSIZE*YSIZE; i++) {
+        lattice[i] = lattice_new[i];
+    }
+}
+
+
+
+int main() {
+    // Allocate host memory
+    int *lattice, *lattice_new;
+    lattice = (int*) calloc(XSIZE * YSIZE, sizeof(int));
+    lattice_new = (int*) calloc(XSIZE * YSIZE, sizeof(int));
+      
+    // Initialise host array
+    srand(time(NULL));
+    for (int i = 1; i < XSIZE-1; i++) {
+        for (int j = 1; j < YSIZE-1; j++) {
+            lattice[i*YSIZE+j] = rand() % 100 < 33;
+        }
+    }
+    
+    // TODO: Start timer
+        
+    // Main simulation loop
+    for (int i = 0; i <= MAXSTEP; i++) {               
+        propagate_and_update(lattice, lattice_new);
+    }
+      
+      
+   // TODO: Stop timer
+   
+   // Deallocate host memory
+   free(lattice);
+   free(lattice_new);
+}
+
